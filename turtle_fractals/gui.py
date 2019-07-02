48fd1214c737090e642b08044dc4549745102ebf
Tushar Khan <tushar.54k@gmail.com>
2019-07-02 14:16:01 -0400

    Added functionality to save new fractals

--- turtle_fractals/gui.py
+++ turtle_fractals/gui.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-from os import listdir
+from os import listdir, path
 from json import dump, load
 from turtle import RawTurtle, TurtleScreen
 from fractals import LFractal, symbol_functions
@@ -12,6 +12,8 @@ by Tushar Khan
 """
 
 class Window():
+
+    fractals_directory = 'saved_fractals'
 
     def __init__(self):
         # Set window and window elements constants
@@ -27,25 +29,23 @@ class Window():
         
         # Create top-level window and add canvas and control panel
         root = tk.Tk()
-        root.title('Turtle Fractals')
+        root.title('Lindenmayer Turtle Fractals')
         self.canvas = tk.Canvas(root, width = canvas_width, height = window_height, cursor = 'crosshair')
         panel = tk.Frame(root, width = panel_width, height = window_height)
         self.canvas.pack(side = 'left', fill = 'both', expand = True)
         panel.pack(side = 'right', fill = 'y', expand = False)
         panel.pack_propagate(False)
 
+        # Center window on screen
+        width = canvas_width + panel_width
+        height = window_height
+        x = (root.winfo_screenwidth() - width)/2
+        y = (root.winfo_screenheight() - height)/2 - 30
+        root.geometry('%dx%d+%d+%d' % (width, height, x, y))
+
         # Create turtle objects
         self.screen = TurtleScreen(self.canvas)
         self.turtle = RawTurtle(self.screen)
-
-        # Load fractals from save files into a dictionary
-        saved_fractals_dir = 'saved_fractals'
-        self.fractal_dict = {}
-        for file in listdir(saved_fractals_dir):
-            if file.endswith('.txt'):
-                with open(saved_fractals_dir + '/' + file) as f:
-                    fractal_tuple = tuple(load(f))
-                    self.fractal_dict[file[:-len('.txt')]] = fractal_tuple
 
         # Add fractal editor frame
         self.editor_frame = tk.Frame(panel, width = panel_width, pady = editor_pad)
@@ -68,9 +68,10 @@ class Window():
         # Add load fractal option menu widget
         self.fractal_var = tk.StringVar()
         self.fractal_var.set('Custom')
-        fractal_menu = tk.OptionMenu(self.editor_frame, self.fractal_var, *list(self.fractal_dict.keys()))
+        fractal_dict = self._generate_fractals_dictionary()
+        self.fractal_menu = tk.OptionMenu(self.editor_frame, self.fractal_var, *list(fractal_dict.keys()))
         self.fractal_var.trace_add('write', callback = self._load_fractal)
-        fractal_menu.grid(row = 1, columnspan = 3, sticky = 'ew')
+        self.fractal_menu.grid(row = 0, column = 1, columnspan = 2, sticky = 'ew')
 
         # Add general editor entry and spinboxes
         self.alphabet_var = tk.StringVar()
@@ -117,7 +118,7 @@ class Window():
         self.speed_var.trace_add('write', callback = lambda *_ : self.screen.tracer(self.speed_var.get(), 10))
         self.iterations_scale = tk.Scale(panel, orient = 'horizontal', from_ = 0, to = 20, label = 'Iterations:',)
         speed_scale = tk.Scale(panel, orient = 'horizontal', from_ = 1, to = 100, label = 'Speed:', variable = self.speed_var)
-        self.size_scale = tk.Scale(panel, orient = 'horizontal', from_ = 1, to = 100, label = 'Unit Length:', resolution = 1)
+        self.size_scale = tk.Scale(panel, orient = 'horizontal', from_ = 1, to = 50, label = 'Unit Length:', resolution = 1)
         self.size_scale.set(25)
         self.iterations_scale.pack(fill = 'x')
         speed_scale.pack(fill = 'x')
@@ -129,9 +130,9 @@ class Window():
         button_frame.columnconfigure(0, weight = 1, minsize = 2/2)
         button_frame.columnconfigure(1, weight = 1, minsize = 2/2)
 
-        self.draw_button = tk.Button(button_frame, text = 'Generate', command = self._draw_fractal)
+        self.draw_button = tk.Button(button_frame, text = 'Draw', command = self._draw_fractal)
         self.reset_button = tk.Button(button_frame, text = 'Reset', command = self._reset_fractal)
-        self.save_fractal_button = tk.Button(button_frame, text = 'Save Fractal', command = self._save_fractal)
+        self.save_fractal_button = tk.Button(button_frame, text = 'Save Fractal', command = self._save_fractal_popup)
         self.save_image_button = tk.Button(button_frame, text = 'Save Image', command = self._save_image)
         self.draw_button.grid(row = 0, column = 0, sticky = 'nsew')
         self.reset_button.grid(row = 0, column = 1, sticky = 'nsew')
@@ -143,18 +144,30 @@ class Window():
         textbox_frame = tk.Frame(panel, pady = textbox_outside_pad, padx = textbox_outside_pad, bg = textbox_bg)
         textbox_frame.pack(side = 'bottom', fill = 'both', expand = True)
         self.textbox_var = tk.StringVar()
-        self.textbox_var.set('Turtle Fractal Drawer')
-        self.textbox_label = tk.Label(textbox_frame, width = panel_width, relief = 'groove', font=('Fixedsys', 7), textvariable = self.textbox_var, wraplength = panel_width - textbox_inside_pad, fg = 'black', bg = textbox_bg)
+        self.textbox_var.set('Lindenmayer Fractals Drawer')
+        self.textbox_label = tk.Label(textbox_frame, width = panel_width, relief = 'groove', font = ('Fixedsys', 7), textvariable = self.textbox_var, wraplength = panel_width - textbox_inside_pad, fg = 'black', bg = textbox_bg)
         self.textbox_var.trace_add('write', callback = lambda *_ : self.textbox_label.configure(text = self.textbox_var.get()))
         self.textbox_label.pack(side = 'bottom', fill = 'both', expand = True)
 
         # Run tkinter loop
         root.mainloop()
 
+    def _generate_fractals_dictionary(self) -> dict:
+        """Loads fractals from save files into a dictionary"""
+        fractal_dict = {}
+        directory = Window.fractals_directory
+        for file in listdir(directory):
+            if file.endswith('.json'):
+                with open(directory + '/' + file) as f:
+                    fractal_tuple = tuple(load(f))
+                    fractal_dict[file[:-len('.json')]] = fractal_tuple
+        return fractal_dict
+
     def _load_fractal(self, *_):
         # Load fractal from fractal dictionary
         fractal = LFractal()
-        fractal.load_tuple(self.fractal_dict[self.fractal_var.get()])
+        fractal_dict = self._generate_fractals_dictionary()
+        fractal.load_tuple(fractal_dict[self.fractal_var.get()])
 
         # Update window elements
         self.alphabet_var.set(fractal.alphabet)
@@ -170,7 +183,7 @@ class Window():
     def _generate_fractal(self) -> LFractal:
         """Returns an LFractal generated from the values specified by the GUI elements"""
         fractal = LFractal()
-        fractal.alphabet = (list(self.alphabet_var.get()))
+        fractal.alphabet = (list(self.alphabet_var.get().replace(' ', '')))
         fractal.axiom(self.axiom_var.get())
         fractal.angle(self.angle_var.get())
         for elem in self.production_rules:
@@ -208,16 +221,67 @@ class Window():
         self.screen.update()
         self.draw_button.configure(state = 'normal')
 
-    # TODO: IMPROVE
     def _reset_fractal(self):
         self.turtle.active = False
         self.screen.reset()
 
-    # TODO: IMPROVE
-    def _save_fractal(self):
-        fractal = self._generate_fractal() 
-        tuple_ = fractal.get_tuple()
-        # TODO get file location to dump tuple (i.e. the name you want to save it by), then dump the tuple
+    def _save_fractal_popup(self):
+        # Create popup window
+        self.save_popup = tk.Toplevel()
+        self.save_popup.title('Save As')
+        self.save_popup.resizable(False, False)
+
+        # Add elements to popup window
+        popup_frame = tk.Frame(self.save_popup, padx = 30, pady = 10)
+        instruction_text = tk.Label(popup_frame, text = 'Enter name for fractal system:', height = 2)
+        self.save_entry = tk.Entry(popup_frame)
+        self.save_button1 = tk.Button(popup_frame, text = 'Done', command = lambda : self._save_fractal(overwrite = False), width = 5)
+        self.save_button2 = tk.Button(popup_frame, text = 'Cancel', command = self.save_popup.destroy, width = 5)
+        self.save_message = tk.Label(popup_frame, width = 41, height = 2)
+
+        # Pack elements into popup window
+        popup_frame.pack(expand = True, fill = tk.BOTH)
+        instruction_text.grid(row = 0, column = 0, columnspan = 2, sticky = 'nsw')
+        self.save_entry.grid(row = 1, column = 0, columnspan = 2, sticky = 'nsew')
+        self.save_button1.grid(row = 2, column = 0, sticky = 'nsew')
+        self.save_button2.grid(row = 2, column = 1, sticky = 'nsew')
+        self.save_message.grid(row = 3, column = 0, columnspan = 2)
+
+    def _save_fractal(self, overwrite = False):
+        directory = Window.fractals_directory
+        fractal_name = ' '.join([word.capitalize() for word in self.save_entry.get().split(' ')])
+        filename = directory + '\\' + fractal_name + '.json'
+
+        print(filename)
+        if fractal_name.replace(' ', '') == '':
+            self.save_message.configure(text = 'You cannot leave the name blank')
+        elif path.exists(filename) and not overwrite:
+            self.save_message.configure(text = 'This fractal already exists.\nWould you like to overwrite its contents?')
+            self.save_entry.configure(state = tk.DISABLED)
+            self.save_button1.configure(text = 'Yes', command = lambda : self._save_fractal(overwrite = True))
+            self.save_button2.configure(text = 'No', command = self.save_fractal_cancel_overwrite)
+        else:
+            # Dump fractal tuple to json file
+            with open(filename, mode = 'w') as f:
+                fractal = self._generate_fractal()
+                dump(fractal.get_tuple(), f)
+
+            # Update fractals menu TODO: update menu to show new fractals
+            fractal_dict = self._generate_fractals_dictionary()
+            self.fractal_menu = tk.OptionMenu(self.editor_frame, self.fractal_var, *list(fractal_dict.keys()))
+
+            # Destroy popup window  
+            self.save_popup.destroy()
+
+            # Update textbox
+            self.textbox_label.configure(fg = 'black')
+            self.textbox_var.set(f'Saved Lindenmayer System as\n{fractal_name}')
+    
+    def save_fractal_cancel_overwrite(self):
+        self.save_message.configure(text = 'Enter a new name for the fractal system')
+        self.save_entry.configure(state = tk.NORMAL)
+        self.save_button1.configure(text = 'Done', command = lambda : self._save_fractal(overwrite = False))
+        self.save_button2.configure(text = 'Cancel', command = self.save_popup.destroy)
 
     # TODO: IMPROVE
     def _save_image(self):
@@ -236,6 +300,9 @@ class Window():
         self.alphabet_entry.delete(0, tk.END)
         self.alphabet_entry.insert(0, ' '.join(new_alphabet))
         self.alphabet_entry.icursor(index)
+
+        # Update axiom
+        self._update_entry(self.axiom_entry, self.axiom_var, _)
 
         # Update production rules
         previous_rules = {}
@@ -280,7 +347,6 @@ class Window():
     def _scroll_move(self, event):
         self.canvas.scan_dragto(event.x, event.y, gain=1)
         print('scroll move')
-        self.screen.screensize(1000, 1000)
 
     def _mouse_scroll(self, event):
         amount = 0.9 if event.delta < 0 else 1.1
