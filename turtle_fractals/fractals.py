452e97db5cc108bf35345233d6d4257416bc9d3f
Tushar Khan <tushar.54k@gmail.com>
2019-07-02 10:49:41 -0400

    Changed stack to save turtle state, not just position

--- turtle_fractals/fractals.py
+++ turtle_fractals/fractals.py
@@ -30,6 +30,8 @@ class LFractal():
 
     def add_character(self, character : str, function = '', production_rule = None):
         """Adds a character to the alphabet and defines its function and production rule"""
+        if character == '' or character == ' ':
+            return
         character = character.upper()
         function = function.upper()
         if production_rule != None: 
@@ -71,22 +73,24 @@ class LFractal():
         # Center turtle using generated sequence; relatively fast algorithm
         sim_x, sim_y = self.turtle.xcor(), self.turtle.ycor()
         sim_heading = self.turtle.heading()
-        sim_pos_stack = []
+        sim_stack = []
         min_x = max_x = sim_x
         min_y = max_y = sim_y
         for character in self.sequence:
             function = self.functions[character]
-            if function == 'DRAW' or function =='MOVE':
+            if function == LFractal.DRAW or function == LFractal.MOVE:
                 sim_x = sim_x + round(size * cos(radians(sim_heading)), 10)
                 sim_y = sim_y + round(size * sin(radians(sim_heading)), 10)
-            elif function == 'RIGHT':
+            elif function == LFractal.RIGHT:
                 sim_heading = (sim_heading - self.angle()) % 360
-            elif function == 'LEFT':
+            elif function == LFractal.LEFT:
                 sim_heading = (sim_heading + self.angle()) % 360
-            elif function == 'SAVE POS':
-                sim_pos_stack.append((sim_x, sim_y))
-            elif function == 'LOAD POS':
-                sim_x, sim_y = sim_pos_stack.pop()
+            elif function == LFractal.SAVE:
+                sim_stack.append(((sim_x, sim_y), sim_heading))
+            elif function == LFractal.LOAD:
+                position, heading = sim_stack.pop()
+                sim_x, sim_y = position
+                sim_heading = heading
             min_x = min(min_x, sim_x)
             max_x = max(max_x, sim_x)
             min_y = min(min_y, sim_y)
@@ -105,28 +109,30 @@ class LFractal():
         unit = size
 
         # Draw fractal using generated sequence
-        position_stack = []
+        stack = []
         for character in self.sequence:
             if not self.turtle.active:
                 self.turtle.reset()
                 raise Exception('Fractal generation forcibly stopped')
             
             function = self.functions[character]
-            if   function == 'DRAW':
+            if   function == LFractal.DRAW:
                 self.turtle.forward(unit)
-            elif function == 'MOVE':
+            elif function == LFractal.MOVE:
                 self.turtle.penup()
                 self.turtle.forward(unit)
                 self.turtle.pendown()
-            elif function == 'RIGHT':
+            elif function == LFractal.RIGHT:
                 self.turtle.right(self._angle)
-            elif function == 'LEFT':
+            elif function == LFractal.LEFT:
                 self.turtle.left(self._angle)
-            elif function == 'SAVE POS':
-                position_stack.append(self.turtle.pos())
-            elif function == 'LOAD POS':
+            elif function == LFractal.SAVE:
+                stack.append((self.turtle.pos(), self.turtle.heading()))
+            elif function == LFractal.LOAD:
                 self.turtle.penup()
-                self.turtle.setposition(position_stack.pop())
+                position, heading = stack.pop()
+                self.turtle.setposition(position)
+                self.turtle.setheading(heading)
                 self.turtle.pendown()
 
         self.time_elapsed = time() - start
@@ -149,8 +155,8 @@ if __name__ == '__main__':
     fractal.add_character('F', 'draw', 'F F + [ + F - F - F ] - [ - F + F + F ]')
     fractal.add_character('+', 'right')
     fractal.add_character('-', 'left')
-    fractal.add_character('[', 'save pos')
-    fractal.add_character(']', 'load pos')
+    fractal.add_character('[', 'save')
+    fractal.add_character(']', 'load')
     fractal.axiom('F')
     fractal.angle(25)
     
